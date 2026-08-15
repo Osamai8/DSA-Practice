26baebec1f1f8430fa787b40249a510657a9bdc7
Osama Islam <islamosama488@gmail.com>
2026-08-15 15:43:43 +0530

    binary search

--- 8.binary-search.js
+++ 8.binary-search.js
@@ -6,3 +6,19 @@
 
 // The catch is that you are not allowed to just look at every element. You have to do it in O(log n) time, 
 // which in practice means you must throw away half of what is left on every single step.
+
+function binarySearch(nums, target) {
+    let lo = 0;
+    let hi = nums.length - 1;
+
+    while (lo <= hi) {
+        const mid = lo + Math.floor((hi - lo) / 2);
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) lo = mid + 1;
+        else hi = mid - 1;
+    }
+    return -1;
+}
+
+const result = binarySearch([-5, 2, 4, 7, 9, 12, 20, 33], 12);
+console.log(result);
