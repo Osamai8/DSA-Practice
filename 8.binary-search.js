39b0125327f9dd2bb4ceb7c287ae919bfad7b9c3
Osama Islam <islamosama488@gmail.com>
2026-08-15 12:28:59 +0530

    binary search

---
+++ 8.binary-search.js
@@ -0,0 +1,8 @@
+// The question, in normal words
+// You are given a list of numbers that is already sorted from smallest to largest, and one more number called the target.
+
+// Find where the target sits in the list and return its position, which is called its index. 
+// If the target is not in the list at all, return -1.
+
+// The catch is that you are not allowed to just look at every element. You have to do it in O(log n) time, 
+// which in practice means you must throw away half of what is left on every single step.
